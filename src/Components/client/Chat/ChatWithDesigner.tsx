f109c78249861beb8d374b07604f6da2f2abe3c6
sirajunnisa <sirajunnisap@gmail.com>
2023-10-18 15:11:20 +0530

    test

--- src/Components/client/Chat/ChatWithDesigner.tsx
+++ src/Components/client/Chat/ChatWithDesigner.tsx
@@ -12,7 +12,7 @@ import { profile } from '../../../Services/client/userData';
 
 function ChatWithDesigner() {
 
-    const ENDPOINT = "https://greendoor.website/"
+    const ENDPOINT = "https://stitchy-inky.vercel.app"
     let socket: any
     socket = io(ENDPOINT)
 
