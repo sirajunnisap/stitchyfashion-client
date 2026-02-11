f6a038b2f7169c7cc3443d977070f78ac26c7368
sirajunnisa <sirajunnisap@gmail.com>
2026-02-11 20:51:35 +0530

    connect to render

--- src/Constants/API.ts
+++ src/Constants/API.ts
@@ -1,13 +1,3 @@
-// export const userAPI = "http://localhost:4000/";
-// export const userAPI = "https://greendoor.website/";
-
-// export const adminAPI = "http://localhost:4000/admin/";
-// export const adminAPI = "https://greendoor.website/admin/";
-
-// export const designerAPI =  "http://localhost:4000/designer/"
-// export const designerAPI =  "https://greendoor.website/designer/"
-
-
 export const userAPI = "https://stitchyfashion-server.onrender.com/";
 export const adminAPI = "https://stitchyfashion-server.onrender.com/admin/";
 export const designerAPI = "https://stitchyfashion-server.onrender.com/designer/";
